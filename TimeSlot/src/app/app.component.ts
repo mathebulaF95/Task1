089e9e644dcdc859eef277a0fbd990580de63d21
mathebulaF95 <39824349+mathebulaF95@users.noreply.github.com>
2022-08-08 00:05:16 -0800

    Update app.component.ts

--- TimeSlot/src/app/app.component.ts
+++ TimeSlot/src/app/app.component.ts
@@ -10,6 +10,11 @@ import {Component} from '@angular/core';
   export class AppComponent {
     title = 'time-slot';
   }
+export class checkSlotAvailability (time,jobLength, date, availability {
+ checkSlotAvailablility (9,1, '2016-05-20', [9, 10, 14, 15, 16, 17]);
+ checkSlotAvailablility (10,1, '2016-05-20', [9, 10, 14, 15, 16, 17]);
+ checkSlotAvailablility (11,1, '2016-05-20', [9, 10, 14, 15, 16, 17]);
+  }
 @Component({
 selector: 'slider-demo',
 templateUrl: 'demo.slider.component.html',
